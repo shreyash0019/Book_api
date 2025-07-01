194d9d7d91a251993db22514faf4c31e06783293
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-07-01 23:42:35 +0530

    Delete book_review_api/book_review directory

--- book_review_api/book_review/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routes import books
-from app.db import engine, Base
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Book Review API")
-
-app.include_router(books.router)
